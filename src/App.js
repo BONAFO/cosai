782d2462500fcbce525a7fa7ec0da3620434cf49
bonafo <bgbcnnvb@gmail.com>
2023-04-05 13:01:50 -0300

    t2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <HashRouter basename="/">
+      <HashRouter basename="/cosai">
           <Routes>
-            <Route path="/" element={<h1>apscjoqjwe2|1</h1>} />
+            <Route path="/cosai" element={<h1>apscjoqjwe2|1</h1>} />
             <Route
-              path="/perro"
+              path="/cosai/perro"
               element={
                 <img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>
               }
